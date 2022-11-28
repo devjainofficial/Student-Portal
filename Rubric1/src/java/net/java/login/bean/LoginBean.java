197c9e9a5c0e2ca8e35486127fabfa3b1145037d
RohitMundra2002 <116022671+RohitMundra2002@users.noreply.github.com>
2022-11-28 11:22:04 +0530

    Add files via upload

---
+++ Rubric1/src/java/net/java/login/bean/LoginBean.java
@@ -0,0 +1,36 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.java.login.bean;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.io.Serializable;
+
+public class LoginBean implements Serializable {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ Rubric1/web/loginsuccess.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : loginsuccess
+    Created on : 23-Nov-2022, 3:43:40 pm
+    Author     : Lenovo
+--%>
+
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+ pageEncoding="ISO-8859-1"%>
+<%@page import="net.java.login.database.*"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
